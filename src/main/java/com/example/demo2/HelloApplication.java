1cfead0240803c1abf027fba28502e399e05530c
ShoNuffEnuff <108638570+ShoNuffEnuff@users.noreply.github.com>
2023-08-10 12:23:51 +0930

    Update HelloApplication.java

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -13,7 +13,11 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 <<<<<<< HEAD
+<<<<<<< HEAD
         stage.setTitle("Victory is mine!");
+=======
+        stage.setTitle("Whoa Stuff!");
+>>>>>>> parent of 9c3e555 (Adding Files to amend project structure.)
 =======
         stage.setTitle("Whoa Stuff!");
 >>>>>>> parent of 9c3e555 (Adding Files to amend project structure.)
